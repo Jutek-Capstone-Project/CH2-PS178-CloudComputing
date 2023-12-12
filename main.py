3a0936e9ed8664564a5ddd2f4d44de9de6983adc
Whs9801 <williamhilmys@gmail.com>
2023-12-12 23:14:48 +0700

    update function

--- main.py
+++ main.py
@@ -7,12 +7,83 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import load_model 
 from sklearn.metrics import silhouette_score
 from math import cos, asin, sqrt, pi, sin, radians, atan2
 import json
 
 app = Flask(__name__)
 
+def jenis_lapangan(df, input_field):
+    if input_field == 'semua':
+        df_semua = df
+        return df_semua
+    elif input_field == 'sepak bola':
+        df_sepakbola = df[df['jenis_sepakbola'] == 1]
+        return df_sepakbola
+    elif input_field == 'voli':
+        df_voli = df[df['jenis_voli'] == 1]
+        return df_voli
+    elif input_field == 'futsal':
+        df_futsal = df[df['jenis_futsal'] == 1]
+        return df_futsal
+    elif input_field == 'badminton':
+        df_badminton = df[df['jenis_badminton'] == 1]
+        return df_badminton
+    elif input_field == 'lainnya':
+        df_basket = df[df['jenis_basket'] == 1]
+        df_tenis = df[df['jenis_tenis'] == 1]
+        df_lainnya = pd.concat([df_basket, df_tenis], axis=0)
+        return df_lainnya
+    else:
+        return "There is nothing"
+
+def distance(lat1, lon1, lat2, lon2):
+    R = 6371.0 # kilometer
+
+    # Convert latitude and longitude from degrees to radians
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+
+    # Haversine formula
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    distance = R * c
+
+    return distance
+
+# Extract recommendations based on a given location
+def recommend_location_neural_network(df, model, scaler, lng, lat):
+    # Standardize the input coordinates
+    input_coordinates = np.array([[lng, lat]])
+    input_coordinates = scaler.transform(input_coordinates)
+
+    # Predict the cluster using the neural network model
+    predicted_cluster = np.argmax(model.predict(input_coordinates))
+
+    # Filter the dataframe based on the predicted cluster
+    cluster_df = df[df['cluster'] == predicted_cluster].copy()
+
+    # Sort the dataframe based on distance (you may use your own distance function here)
+    cluster_df['distance'] = cluster_df.apply(lambda x: distance(lat, lng, x['lat'], x['lng']), axis=1)
+    sorted_df = cluster_df.sort_values(by=['distance'])
+
+    # Return the top 10 recommendations
+    recommendations = sorted_df.iloc[0:10][['name', 'lng', 'lat', 'distance']]
+    
+    # If the reccomendations less than 10 fields
+    if len(recommendations) < 10:
+        additional_recommendations = df[df['cluster'] != predicted_cluster].copy()
+        additional_recommendations['distance'] = additional_recommendations.apply(lambda x: distance(lat, lng, x['lat'], x['lng']), axis=1)
+        sorted_additional = additional_recommendations.sort_values(by=['distance'])
+
+        additional_recommendations = sorted_additional.iloc[0:(8 - len(recommendations))][['name', 'lng', 'lat', 'distance']]
+
+        recommendations = pd.concat([recommendations, additional_recommendations])
+
+    return recommendations
+             
 def homepage_recommend(lng, lat, input_field):
     # Load dataset
     df = pd.read_csv('./Dataset/Data Lapangan.csv')
@@ -35,80 +106,10 @@ def homepage_recommend(lng, lat, input_field):
     # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    from tensorflow.keras.models import load_model
     model = load_model('new-user-location.h5')
 
-    def jenis_lapangan(input_field):
-        if input_field == 'semua':
-            df_semua = df
-            return df_semua
-        elif input_field == 'sepak bola':
-            df_sepakbola = df[df['jenis_sepakbola'] == 1]
-            return df_sepakbola
-        elif input_field == 'voli':
-            df_voli = df[df['jenis_voli'] == 1]
-            return df_voli
-        elif input_field == 'futsal':
-            df_futsal = df[df['jenis_futsal'] == 1]
-            return df_futsal
-        elif input_field == 'badminton':
-            df_badminton = df[df['jenis_badminton'] == 1]
-            return df_badminton
-        elif input_field == 'lainnya':
-            df_basket = df[df['jenis_basket'] == 1]
-            df_tenis = df[df['jenis_tenis'] == 1]
-            df_lainnya = pd.concat([df_basket, df_tenis], axis=0)
-            return df_lainnya
-        else:
-            return "There is nothing"
-
-    lapangan_df = jenis_lapangan(input_field)
-
-    def distance(lat1, lon1, lat2, lon2):
-        R = 6371.0 # kilometer
-
-        # Convert latitude and longitude from degrees to radians
-        lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
-
-        # Haversine formula
-        dlat = lat2 - lat1
-        dlon = lon2 - lon1
-        a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-        c = 2 * atan2(sqrt(a), sqrt(1 - a))
-        distance = R * c
-
-        return distance
-
-    # Extract recommendations based on a given location
-    def recommend_location_neural_network(df, model, scaler, lng, lat):
-        # Standardize the input coordinates
-        input_coordinates = np.array([[lng, lat]])
-        input_coordinates = scaler.transform(input_coordinates)
-
-        # Predict the cluster using the neural network model
-        predicted_cluster = np.argmax(model.predict(input_coordinates))
-
-        # Filter the dataframe based on the predicted cluster
-        cluster_df = df[df['cluster'] == predicted_cluster].copy()
-
-        # Sort the dataframe based on distance (you may use your own distance function here)
-        cluster_df['distance'] = cluster_df.apply(lambda x: distance(lat, lng, x['lat'], x['lng']), axis=1)
-        sorted_df = cluster_df.sort_values(by=['distance'])
-
-        # Return the top 10 recommendations
-        recommendations = sorted_df.iloc[0:10][['name', 'lng', 'lat', 'distance']]
-        
-        # If the reccomendations less than 10 fields
-        if len(recommendations) < 10:
-            additional_recommendations = df[df['cluster'] != predicted_cluster].copy()
-            additional_recommendations['distance'] = additional_recommendations.apply(lambda x: distance(lat, lng, x['lat'], x['lng']), axis=1)
-            sorted_additional = additional_recommendations.sort_values(by=['distance'])
-
-            additional_recommendations = sorted_additional.iloc[0:(8 - len(recommendations))][['name', 'lng', 'lat', 'distance']]
-
-            recommendations = pd.concat([recommendations, additional_recommendations])
+    lapangan_df = jenis_lapangan(df, input_field)
     
-        return recommendations
 
     recommendations = recommend_location_neural_network(lapangan_df, model, scaler, lng, lat)
 
@@ -123,10 +124,24 @@ def recommend():
     data = request.get_json()
 
     # Extract input parameters from the JSON data
+    df = pd.read_csv('./Dataset/Data Lapangan.csv')
     lng = float(data['lng'])
     lat = float(data['lat'])
-
+    input_field = data['input_field']
+    
     # Perform recommendations
+    scaler = StandardScaler()
+    coordinates = df[['lng', 'lat']]
+    X = coordinates.values
+    X = scaler.fit_transform(X)
+    
+    kmeans = KMeans(n_clusters=10, init='k-means++', random_state=42)
+    df['cluster'] = kmeans.fit_predict(coordinates)
+    
+    model = load_model('new-user-location.h5')
+    lapangan_df = jenis_lapangan(df, input_field)
+    
+    
     recommendations = recommend_location_neural_network(lapangan_df, model, scaler, lng, lat)
 
     # Convert recommendations to JSON
--- user-baru-based-location.py
+++ user-baru-based-location.py
@@ -8,9 +8,48 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from sklearn.metrics import silhouette_score
 from math import cos, asin, sqrt, pi, sin, radians, atan2
+from tensorflow.keras.models import load_model
 import json
 
+def jenis_lapangan(df, input_field):
+    if input_field == 'semua':
+        df_semua = df
+        return df_semua
+    elif input_field == 'sepak bola':
+        df_sepakbola = df[df['jenis_sepakbola'] == 1]
+        return df_sepakbola
+    elif input_field == 'voli':
+        df_voli = df[df['jenis_voli'] == 1]
+        return df_voli
+    elif input_field == 'futsal':
+        df_futsal = df[df['jenis_futsal'] == 1]
+        return df_futsal
+    elif input_field == 'badminton':
+        df_badminton = df[df['jenis_badminton'] == 1]
+        return df_badminton
+    elif input_field == 'lainnya':
+        df_basket = df[df['jenis_basket'] == 1]
+        df_tenis = df[df['jenis_tenis'] == 1]
+        df_lainnya = pd.concat([df_basket, df_tenis], axis=0)
+        return df_lainnya
+    else:
+        return "There is nothing"
 
+def distance(lat1, lon1, lat2, lon2):
+        R = 6371.0 # kilometer
+
+        # Convert latitude and longitude from degrees to radians
+        lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+
+        # Haversine formula
+        dlat = lat2 - lat1
+        dlon = lon2 - lon1
+        a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        distance = R * c
+
+        return distance
+        
 def homepage_recommend(lng, lat, input_field):
     # Load dataset
     df = pd.read_csv('./Dataset/Data Lapangan.csv')
@@ -33,81 +72,9 @@ def homepage_recommend(lng, lat, input_field):
     # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    from tensorflow.keras.models import load_model
     model = load_model('new-user-location.h5')
 
-    def jenis_lapangan(input_field):
-        if input_field == 'semua':
-            df_semua = df
-            return df_semua
-        elif input_field == 'sepak bola':
-            df_sepakbola = df[df['jenis_sepakbola'] == 1]
-            return df_sepakbola
-        elif input_field == 'voli':
-            df_voli = df[df['jenis_voli'] == 1]
-            return df_voli
-        elif input_field == 'futsal':
-            df_futsal = df[df['jenis_futsal'] == 1]
-            return df_futsal
-        elif input_field == 'badminton':
-            df_badminton = df[df['jenis_badminton'] == 1]
-            return df_badminton
-        elif input_field == 'lainnya':
-            df_basket = df[df['jenis_basket'] == 1]
-            df_tenis = df[df['jenis_tenis'] == 1]
-            df_lainnya = pd.concat([df_basket, df_tenis], axis=0)
-            return df_lainnya
-        else:
-            return "There is nothing"
-
-    lapangan_df = jenis_lapangan(input_field)
-
-    def distance(lat1, lon1, lat2, lon2):
-        R = 6371.0 # kilometer
-
-        # Convert latitude and longitude from degrees to radians
-        lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
-
-        # Haversine formula
-        dlat = lat2 - lat1
-        dlon = lon2 - lon1
-        a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-        c = 2 * atan2(sqrt(a), sqrt(1 - a))
-        distance = R * c
-
-        return distance
-
-    # Extract recommendations based on a given location
-    def recommend_location_neural_network(df, model, scaler, lng, lat):
-        # Standardize the input coordinates
-        input_coordinates = np.array([[lng, lat]])
-        input_coordinates = scaler.transform(input_coordinates)
-
-        # Predict the cluster using the neural network model
-        predicted_cluster = np.argmax(model.predict(input_coordinates))
-
-        # Filter the dataframe based on the predicted cluster
-        cluster_df = df[df['cluster'] == predicted_cluster].copy()
-
-        # Sort the dataframe based on distance (you may use your own distance function here)
-        cluster_df['distance'] = cluster_df.apply(lambda x: distance(lat, lng, x['lat'], x['lng']), axis=1)
-        sorted_df = cluster_df.sort_values(by=['distance'])
-
-        # Return the top 10 recommendations
-        recommendations = sorted_df.iloc[0:10][['name', 'lng', 'lat', 'distance']]
-        
-        # If the reccomendations less than 10 fields
-        if len(recommendations) < 10:
-            additional_recommendations = df[df['cluster'] != predicted_cluster].copy()
-            additional_recommendations['distance'] = additional_recommendations.apply(lambda x: distance(lat, lng, x['lat'], x['lng']), axis=1)
-            sorted_additional = additional_recommendations.sort_values(by=['distance'])
-
-            additional_recommendations = sorted_additional.iloc[0:(8 - len(recommendations))][['name', 'lng', 'lat', 'distance']]
-
-            recommendations = pd.concat([recommendations, additional_recommendations])
-    
-        return recommendations
-
+    lapangan_df = jenis_lapangan(df, input_field)
     recommendations = recommend_location_neural_network(lapangan_df, model, scaler, lng, lat)
 
     output = recommendations.to_dict(orient='records')
@@ -115,3 +82,36 @@ def homepage_recommend(lng, lat, input_field):
 
     print(json_output)
     return json.loads(json_output)
+
+# Extract recommendations based on a given location
+def recommend_location_neural_network(df, model, scaler, lng, lat):
+    # Standardize the input coordinates
+    input_coordinates = np.array([[lng, lat]])
+    input_coordinates = scaler.transform(input_coordinates)
+
+    # Predict the cluster using the neural network model
+    predicted_cluster = np.argmax(model.predict(input_coordinates))
+
+    # Filter the dataframe based on the predicted cluster
+    cluster_df = df[df['cluster'] == predicted_cluster].copy()
+
+    # Sort the dataframe based on distance (you may use your own distance function here)
+    cluster_df['distance'] = cluster_df.apply(lambda x: distance(lat, lng, x['lat'], x['lng']), axis=1)
+    sorted_df = cluster_df.sort_values(by=['distance'])
+
+    # Return the top 10 recommendations
+    recommendations = sorted_df.iloc[0:10][['name', 'lng', 'lat', 'distance']]
+    
+    # If the reccomendations less than 10 fields
+    if len(recommendations) < 10:
+        additional_recommendations = df[df['cluster'] != predicted_cluster].copy()
+        additional_recommendations['distance'] = additional_recommendations.apply(lambda x: distance(lat, lng, x['lat'], x['lng']), axis=1)
+        sorted_additional = additional_recommendations.sort_values(by=['distance'])
+
+        additional_recommendations = sorted_additional.iloc[0:(8 - len(recommendations))][['name', 'lng', 'lat', 'distance']]
+
+        recommendations = pd.concat([recommendations, additional_recommendations])
+
+    return recommendations
+
+
