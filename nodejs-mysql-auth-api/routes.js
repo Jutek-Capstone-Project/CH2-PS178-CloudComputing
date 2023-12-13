d3767a57551afd1ba64f6f564178822b404cc971
williamhilmysusatyo <william.hilmy.susatyo@mail.ugm.ac.id>
2023-12-13 00:28:32 +0000

    update progress

--- nodejs-mysql-auth-api/routes.js
+++ nodejs-mysql-auth-api/routes.js
@@ -6,13 +6,72 @@ import express from 'express';
 import csv from 'csv-parser';
 import fs from 'fs';
 import DB from './dbConnection.js'; 
+import {jenis_lapangan, distance, indexOfMax} from './sample.js';
+import {kmeans} from 'ml-kmeans';
+import tfn from '@tensorflow/tfjs-node';
+import * as tf from '@tensorflow/tfjs';
 
 const bucketName = 'dataset-bucket-9801';
-const fileName = 'dataset_cleaned_25Nov.csv';
+const fileName = 'Data Lapangan.csv';
 
 const routes = Router({ strict: true });
 const storage = new Storage();
 const bucket = storage.bucket(bucketName);
+
+const load_model = async (results, X, y, lng, lat) => {
+    const handler = 'https://github.com/Jutek-Capstone-Project/CH2-PS178-Cloud_Computing/blob/branch-2/model/new-user-location.tfjs/model.json';
+    const model = await tf.loadLayersModel(handler);
+    const coord = tf.tensor([[lng, lat]]);
+    const predicted_cluster = indexOfMax(model.predict(coord));
+    console.log(predicted_cluster);
+    console.log(X.length);
+    console.log(y.length);
+    const cluster = []
+    for(let i=0; i<y.length; i++){
+        if(y[i] == predicted_cluster){
+            cluster.push(X[i]);
+        }
+    }
+    console.log(cluster.length);
+    const dist = [];
+    const index = [];
+    for(let i=0; i<cluster.length; i++){
+      dist.push(distance(lat, lng, cluster[i][1], cluster[i][0]));
+    }
+    const val = dist.sort().slice(0,10);
+    for(let i=0; i<val.length; i++){
+      for(let j=0; j<cluster.length; j++){
+        if(val[i] == distance(lat, lng, cluster[j][1], cluster[j][0])){
+          index.push(results[j]);
+        }
+      }
+    }   
+    console.log(index.length);
+    return index;
+}
+
+const homepage_recommend = (results, longitude, latitude) => {
+    const X = new Array();
+
+    for(let i=0; i<results.length; i++){
+        X[i] = [results[i].lng, results[i].lat];
+    }
+
+    let centers = [[ 110.51120340789475, -7.9387781868421055],
+    [110.122775775, -7.8736944675],
+    [110.22179131707321, -7.90374066097561],
+    [110.41456638157892, -7.708609292105262],
+    [110.2922045422222, -7.773480315555553],
+    [110.32779292745096, -7.940743580392157],
+    [110.40239900615383, -7.835370053846154],
+    [110.17963109249999, -7.749816457500001],
+    [110.64924044651166, -7.905393716279071],
+    [110.682575696875, -8.069387253125]]
+
+    let out = kmeans(X, 10, { initialization: centers })
+    let y = out.clusters;
+    return [X,y];
+}
 // Token Validation Rule
 const tokenValidation = (isRefresh = false) => {
     let refreshText = isRefresh ? 'Refresh' : 'Authorization';
@@ -128,4 +187,27 @@ routes.get('/dbdata', (req, res) => {
     }
     });
 
+routes.post('/model', async(req, res) => {
+    try{
+        let { lng, lat, input_field} = req.body;
+        let results = []
+
+        const file = bucket.file(fileName);
+
+        file.createReadStream()
+        .pipe(csv())
+        .on('data', (data) => {results.push(data);})
+
+        .on('end', async() => {
+        let output = jenis_lapangan(input_field, results);
+        let [x,y] = homepage_recommend(output, lng, lat);
+        let final = await load_model(results, x, y, lng, lat);
+        res.json(final);
+        });
+    }
+
+    catch(err){
+    res.status(500).json({ error: 'An error occurred .' })
+    }
+});
 export default routes;
---
+++ nodejs-mysql-auth-api/sample.js
@@ -0,0 +1,110 @@
+import { sin, cos, sqrt, atan2 } from 'mathjs';
+
+export const jenis_lapangan = (input_field, results) => {
+    if (input_field == 'semua'){
+        const df_semua = results;
+        return df_semua;
+    }
+    else if(input_field == 'sepak bola')
+    {
+    const df_sepakbola = []; 
+    for(let i=0; i<results.length; i++){
+        if(results[i].jenis_sepakbola == 1){
+            df_sepakbola.push(results[i]);
+        }
+    } 
+        return df_sepakbola;
+    }
+    else if(input_field == 'voli')
+    {
+    const df_voli = []; 
+    for(let i=0; i<results.length; i++){
+        if(results[i].jenis_voli == 1){
+            df_voli.push(results[i]);
+        }
+    } 
+        return df_voli;
+    }
+    else if(input_field == 'futsal')
+    {
+    const df_futsal = []; 
+    for(let i=0; i<results.length; i++){
+        if(results[i].jenis_futsal == 1){
+            df_futsal.push(results[i]);
+        }
+    } 
+        return df_futsal;
+    }
+    else if(input_field == 'badminton')
+    {
+    const df_badminton = []; 
+    for(let i=0; i<results.length; i++){
+        if(results[i].jenis_badminton == 1){
+            df_badminton.push(results[i]);
+        }
+    } 
+        return df_badminton;
+    }
+    else if(input_field == 'basket')
+    {
+    const df_basket = []; 
+    for(let i=0; i<results.length; i++){
+        if(results[i].jenis_basket == 1){
+            df_basket.push(results[i]);
+        }
+    } 
+        return df_basket;
+    }
+    else if(input_field == 'tenis')
+    {
+    const df_tenis = []; 
+    for(let i=0; i<results.length; i++){
+        if(results[i].jenis_tenis == 1){
+            df_tenis.push(results[i]);
+        }
+    } 
+        return df_tenis;
+    }
+    else{
+        return 'There is nothing';
+    }
+
+}
+
+export const distance = (lat1, lon1, lat2, lon2) => {
+    let R = 6371;
+
+    function degrees_to_radians(degrees)
+    {
+    var pi = Math.PI;
+    return degrees * (pi/180);
+    }
+
+    lat1 = degrees_to_radians(lat1);
+    lat2 = degrees_to_radians(lat2);
+    lon1 = degrees_to_radians(lon1);    
+    lon2 = degrees_to_radians(lon2);    
+
+    let dlat = lat2 - lat1;
+    let dlon = lon2 - lon1;
+    let a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    let c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    let distance = R * c
+
+    return distance
+}
+
+export const indexOfMax = (arr) => {
+    if (arr.length === 0) {
+        return -1;
+    }
+    var max = arr[0];
+    var maxIndex = 0;
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            maxIndex = i;
+            max = arr[i];
+        }
+    }
+    return maxIndex;
+}
