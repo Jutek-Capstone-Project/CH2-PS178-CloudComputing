86b27783d5b6fa77685ad463bae7aff9d75709df
williamhilmysusatyo <william.hilmy.susatyo@mail.ugm.ac.id>
2024-01-08 07:31:54 +0000

    update routes, controller, and libraries

--- nodejs-mysql-auth-api/routes.js
+++ nodejs-mysql-auth-api/routes.js
@@ -10,19 +10,25 @@ import {jenis_lapangan, distance, indexOfMax} from './sample.js';
 import {kmeans} from 'ml-kmeans';
 import tfn from '@tensorflow/tfjs-node';
 import * as tf from '@tensorflow/tfjs';
+import Stripe from 'stripe';
+import Midtrans from 'midtrans-client';
+
 
 const bucketName = 'dataset-bucket-9801';
-const fileName = 'Data Lapangan.csv';
-
+const fileName = 'resolved.csv';
+const stripe = new Stripe('sk_test_51OLggCDVadqvWMEH2xy5NwCb0ViTaYZ2FRKBF4axp8lNZnlSH7PQsRMnLyC1u1O5Bg8XUtEG4FAYRCCxyw0zlaLm00J9fBHnqE');
 const routes = Router({ strict: true });
 const storage = new Storage();
 const bucket = storage.bucket(bucketName);
 
+const snap = new Midtrans.Snap({
+    isProduction: false,
+    serverKey: 'SB-Mid-server-o_jLWrEdKiLLkbniZFShocWw',
+    clientKey: 'SB-Mid-client-0HADXNqHW92380qb',
+  });
+
 const load_model = async (results, X, y, lng, lat) => {
-    const handler = 'https://github.com/Jutek-Capstone-Project/CH2-PS178-Cloud_Computing/blob/branch-2/model/new-user-location.tfjs/model.json';
-    const model = await tf.loadLayersModel(handler);
-    const coord = tf.tensor([[lng, lat]]);
-    const predicted_cluster = indexOfMax(model.predict(coord));
+    const predicted_cluster = 0;
     console.log(predicted_cluster);
     console.log(X.length);
     console.log(y.length);
@@ -38,7 +44,7 @@ const load_model = async (results, X, y, lng, lat) => {
     for(let i=0; i<cluster.length; i++){
       dist.push(distance(lat, lng, cluster[i][1], cluster[i][0]));
     }
-    const val = dist.sort().slice(0,10);
+    const val = dist.sort().slice(0,4);
     for(let i=0; i<val.length; i++){
       for(let j=0; j<cluster.length; j++){
         if(val[i] == distance(lat, lng, cluster[j][1], cluster[j][0])){
@@ -50,7 +56,7 @@ const load_model = async (results, X, y, lng, lat) => {
     return index;
 }
 
-const homepage_recommend = (results, longitude, latitude) => {
+const homepage_recommend = (results) => {
     const X = new Array();
 
     for(let i=0; i<results.length; i++){
@@ -176,10 +182,36 @@ routes.get('/data', (req, res) => {
   });
 
 
+routes.post('/data-paging', (req, res) => {
+    const {page, pageSize} = req.body;
+    const pageNumber = parseInt(page) || 1;
+    const size = parseInt(pageSize) || 10;
 
-routes.get('/dbdata', (req, res) => {
+    const data = []
+    const total = data.length/size;
+    let rowCounter = 0;
+    let dict = {};
+    const file = bucket.file(fileName);
+    file.createReadStream()
+      .pipe(csv())
+      .on('data', (row) => {
+        rowCounter++;
+        if(rowCounter > (pageNumber-1)*size && rowCounter <= pageNumber*size){
+            data.push(row);
+        }
+      })
+      .on('end', () => {
+        dict['data'] = data;
+        dict['page'] = page;
+        dict['pageSize'] = pageSize;
+        dict['totalItems'] = rowCounter;
+        res.json(dict);
+      })
+})
+
+routes.get('/dbdata', async (req, res) => {
     try{
-        const [result] = DB.execute('SELECT * FROM `users`');
+        const result = await DB.execute('SELECT * FROM `users`');
         res.json(result)
     }
     catch(err){
@@ -200,7 +232,7 @@ routes.post('/model', async(req, res) => {
 
         .on('end', async() => {
         let output = jenis_lapangan(input_field, results);
-        let [x,y] = homepage_recommend(output, lng, lat);
+        let [x,y] = homepage_recommend(output);
         let final = await load_model(results, x, y, lng, lat);
         res.json(final);
         });
@@ -210,4 +242,48 @@ routes.post('/model', async(req, res) => {
     res.status(500).json({ error: 'An error occurred .' })
     }
 });
+
+routes.post('/process', async(req, res) => {
+    const { amount, currency } = req.body;
+
+    try {
+        // Create a payment intent using the Stripe API
+        const paymentIntent = await stripeInstance.paymentIntents.create({
+        amount,
+        currency,
+        confirm: true,
+        });
+
+        // Return the payment intent status to the client
+        res.json({ status: paymentIntent.status });
+
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred while processing the payment.' });
+    }
+});
+
+routes.post('/create-midtrans-payment', async (req, res) => {
+    const { id, productName, price, quantity} = req.body;
+
+    // Create a Snap transaction token
+    let parameter = {
+        "item_details": [{
+            "name": productName,
+            "price": price,
+            "quantity": quantity
+        }],
+        "transaction_details": {
+            "order_id": id,
+            "gross_amount": price*quantity
+        }
+    }
+
+    const token = await snap.createTransactionToken(parameter);
+    console.log(token)
+    res.json({token})
+
+      }
+    );
+
+  
 export default routes;
