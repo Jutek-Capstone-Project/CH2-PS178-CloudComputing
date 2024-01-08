df23d4177e981b6dc2debffb6285fc38f2354f91
williamhilmysusatyo <william.hilmy.susatyo@mail.ugm.ac.id>
2024-01-08 07:40:01 +0000

    update flask env

--- main.py
+++ main.py
@@ -3,16 +3,25 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.models import load_model 
 from sklearn.metrics import silhouette_score
+from io import BytesIO
+from google.cloud import storage
+import gcsfs
 from math import cos, asin, sqrt, pi, sin, radians, atan2
+from tensorflow.python.lib.io import file_io
 import json
+import os 
 
 app = Flask(__name__)
+
+storage_client = storage.Client()
+bucket = storage_client.get_bucket('streamlit-bucket9801')
 
 def jenis_lapangan(df, input_field):
     if input_field == 'semua':
@@ -83,72 +92,103 @@ def recommend_location_neural_network(df, model, scaler, lng, lat):
         recommendations = pd.concat([recommendations, additional_recommendations])
 
     return recommendations
-             
-def homepage_recommend(lng, lat, input_field):
-    # Load dataset
-    df = pd.read_csv('./Dataset/Data Lapangan.csv')
-
-    # Extract coordinates
-    coordinates = df[['lng', 'lat']]
-
-    # Perform KMeans clustering
-    kmeans = KMeans(n_clusters=10, init='k-means++', random_state=42)
-    df['cluster'] = kmeans.fit_predict(coordinates)
-
-    # Define features and target
-    X = coordinates.values
-    y = df['cluster'].values
-
-    # Standardize the data
-    scaler = StandardScaler()
-    X = scaler.fit_transform(X)
-
-    # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    model = load_model('new-user-location.h5')
-
-    lapangan_df = jenis_lapangan(df, input_field)
-    
-
-    recommendations = recommend_location_neural_network(lapangan_df, model, scaler, lng, lat)
-
-    output = recommendations.to_dict(orient='records')
-    json_output = json.dumps(output, indent = 4)
-
-    print(json_output)
-    return json.loads(json_output)
 
 @app.route('/recommend', methods=['POST'])
 def recommend():
     data = request.get_json()
 
-    # Extract input parameters from the JSON data
-    df = pd.read_csv('./Dataset/Data Lapangan.csv')
+    df = pd.read_csv('gs://streamlit-bucket9801/Data Lapangan (2).csv')
     lng = float(data['lng'])
     lat = float(data['lat'])
     input_field = data['input_field']
     
-    # Perform recommendations
     scaler = StandardScaler()
     coordinates = df[['lng', 'lat']]
     X = coordinates.values
     X = scaler.fit_transform(X)
     
-    kmeans = KMeans(n_clusters=10, init='k-means++', random_state=42)
+    kmeans = KMeans(n_clusters=4, init='k-means++', random_state=42)
     df['cluster'] = kmeans.fit_predict(coordinates)
     
-    model = load_model('new-user-location.h5')
+    model = load_model('gs://streamlit-bucket9801/new-user-location (2).h5')
     lapangan_df = jenis_lapangan(df, input_field)
     
     
     recommendations = recommend_location_neural_network(lapangan_df, model, scaler, lng, lat)
 
-    # Convert recommendations to JSON
     output = recommendations.to_dict(orient='records')
     json_output = json.dumps(output, indent=4)
 
     return jsonify(json.loads(json_output))
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=3000, debug=True)
+@app.route('/recommend-similar-field', methods=['POST'])
+def recommend_similar_field():
+    data = request.get_json()
+    current_lng = float(data['lng'])
+    current_lat = float(data['lat'])
+    input_field = data['input_field']
+
+    field_dataset = pd.read_csv('gs://streamlit-bucket9801/Data Lapangan (2).csv')
+
+    row_condition = field_dataset['id'] == input_field
+    row_index = field_dataset.index[row_condition][0]
+    columns_with_1 = field_dataset.iloc[row_index, 1:] == 1
+
+    columns = ['jenis_sepakbola', 'jenis_badminton', 'jenis_tenis', 'jenis_futsal', 'jenis_voli', 'jenis_basket']
+    for i in columns:
+        if columns_with_1[i]:
+            jenis = i
+            break
+
+    pca_field = field_dataset[['lat', 'lng', 'rating', 'price', jenis,
+                               'fasilitas_wifi', 'fasilitas_parkir_motor', 'fasilitas_parkir_mobil',
+                               'fasilitas_wc', 'fasilitas_kantin', 'fasilitas_mushola']]
+
+    pca_field = pca_field[pca_field[jenis] == 1]
+
+    # Standardize the data
+    scaler = StandardScaler()
+    X_scaled = pd.DataFrame(scaler.fit_transform(pca_field), columns=pca_field.columns, index=pca_field.index)
+
+
+    autoencoder = load_model('gs://streamlit-bucket9801/pca-autoencoder.h5')
+    encoded_data = autoencoder.predict(X_scaled)
+    X_pca_tensor = tf.convert_to_tensor(encoded_data, dtype=tf.float32)
+
+
+    # KMeans clustering
+    n_clusters = 4
+    initial_clusters = tf.random.shuffle(X_pca_tensor)[:n_clusters]
+    kmeans = tf.compat.v1.estimator.experimental.KMeans(num_clusters=n_clusters, use_mini_batch=False, seed = 0)
+
+    def input_fn():
+        return tf.compat.v1.train.limit_epochs(tf.convert_to_tensor(encoded_data, dtype=tf.float32), num_epochs=1)
+
+    previousCenters = initial_clusters
+    for _ in range(2):
+        kmeans.train(input_fn)
+        clusterCenters = kmeans.cluster_centers()
+        previousCenters = clusterCenters
+
+    clusterCenters = kmeans.cluster_centers()
+    clusterLabels = list(kmeans.predict_cluster_index(input_fn))
+
+    pca_field['cluster'] = clusterLabels
+    cluster = pca_field.iloc[row_index]['cluster']
+
+    rec_index = pca_field.index[pca_field['cluster'] == cluster].tolist()
+    list_recommend = field_dataset.iloc[rec_index]
+
+    list_recommend['dist'] = list_recommend.apply(lambda x: distance(current_lat, current_lng, x['lat'], x['lng']), axis=1)
+
+    shortest_distance = list_recommend.sort_values(by=['dist'], ascending=True).iloc[0:10]
+
+    output = shortest_distance.to_dict(orient='records')
+    json_output = json.dumps(output, indent = 4)
+
+    print(json_output)
+    return json.loads(json_output)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
